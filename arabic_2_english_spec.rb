9b8d56501583b06301854404b544fa2f1b8a1903
Orferus <david.nvalderrama@gmail.com>
2017-11-17 19:42:30 -0500

    Add first approach specs

---
+++ arabic_2_english_spec.rb
@@ -0,0 +1,34 @@
+require_relative 'arabic_2_english'
+
+describe Arabic2English do
+  context "The number has three or less digits" do
+    context "the number contains eleven or twelve" do
+      it "should return the numeral with it's delimiter" do
+        expect(5.to_english).to eq("five")
+        expect(11.to_english).to eq("eleven")
+        expect(12.to_english).to eq("twelve")
+        expect(311.to_english).to eq("three hundred and eleven")
+      end
+    end
+    context "the number it's a regular base root" do
+      it "should return the numeral with hundreds delimiter" do
+        expect(100.to_english).to eq("one hundred")
+        expect(522.to_english).to eq("five hundred and twenty-two")
+      end
+    end
+  end
+  context "The number has more than three digits" do
+    context "the number contains eleven or twelve" do
+      it "should return the numeral with its respective delimiters and the special case" do
+        expect(1111.to_english).to eq("one thousand and one hundred and eleven")
+        expect(2212.to_english).to eq("two thousand and two hundred and twelve")
+      end
+    end
+    context "the number it's a regular base root" do
+      it "should return the numeral with hundreds delimiter" do
+        expect(2222.to_english).to eq("two thousand and two hundred and twenty-two")
+        expect(22222.to_english).to eq("twenty-two thousand and two hundred and twenty-two")
+      end
+    end
+  end
+end

e5f2a834688219cdf2be905a8877614cacb1d4b4
Orferus <david.nvalderrama@gmail.com>
2017-11-22 20:13:25 -0500

    Final clean code exercise

--- arabic_2_english.rb
+++ arabic_2_english.rb
@@ -12,12 +12,8 @@ module Arabic2English
     8 => {singular: "eight", plural: "eighteen", root: "eighty"},
     9 => {singular: "nine", plural: "nineteen", root: "ninety"}
   }
-
-  DELIMITERS = ["", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion"]
-
-  TWO_DIGITS_NUMERALS = {
-    11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"
-  }
+  DELIMITERS = [" thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion"]
+  TWO_DIGITS_NUMERALS = { 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
 
   def to_english
     numerals = get_my_numerals
@@ -25,6 +21,7 @@ module Arabic2English
   end
 
   private
+  #Creator method
   def get_my_numerals
     [].tap do |array|
       get_numbers_collection.each_with_index do |number, index|
@@ -36,60 +33,85 @@ module Arabic2English
     end
   end
 
+  #========================= Begin: High level Flow methods =====================================================
+
   def transform_number_into_numeral(number)
     paths = {
-      1 => lambda { get_one_digit_numeral(number: number) },
-      2 => lambda { get_two_digits_numeral(number: number) },
-      3 => lambda { get_three_digits_numeral(number: number) }
+      1 => lambda { get_one_digit_numeral(number) },
+      2 => lambda { get_two_digits_numeral(number) },
+      3 => lambda { get_three_digits_numeral(number) }
     }
     paths[number.length].call
   end
 
-  #This is in order to get the delimiter and reuse code.
-  def get_numbers_collection
-    self.to_s.reverse.each_char.each_slice(3).map {|x| x.reverse.join }
-  end
-
-  def get_one_digit_numeral(number:)
+  def get_one_digit_numeral(number)
     NUMERALS[number.to_i][:singular]
   end
 
-  def get_two_digits_numeral(number:, prefix: nil)
+  def get_two_digits_numeral(number, prefix: nil)
     parsed_number = verify_special_cases(number)
-    if prefix
-      "#{prefix} and #{parsed_number}"
-    else
-      parsed_number
-    end
+    return number_with_separator(parsed_number, prefix: prefix, separator: " and ") if prefix
+    parsed_number
   end
 
-  def get_three_digits_numeral(number:)
-    number_for_prefix = number[0].to_i
+  def get_three_digits_numeral(number)
+    prefix = prefix_for_three_digits_number(number[0].to_i)
     remaining_number = number[1..2]
-    prefix = prefix_for_three_digits_number(number_for_prefix)
-    prefixed_three_digit_number(prefix, remaining_number)
+    prefixed_three_digit_number(prefix: prefix, remaining_number: remaining_number)
   end
+
+  #========================= End: High level Flow methods =====================================================
+
+  #========================= Begin: Low level Flow methods ====================================================
+
+  def verify_special_cases(number)
+    plural_number = TWO_DIGITS_NUMERALS[number.to_i]
+    return plural_number if plural_number
+    define_two_digit_number(number)
+  end
+
+  def define_two_digit_number(number)
+    _root = NUMERALS[number[0].to_i][:root]
+    return _root if is_it_zero?(number[1])
+    parse_two_digits_with_root(number, root: _root)
+  end
+
+  def prefixed_three_digit_number(prefix: _prefix, remaining_number: _remaining_number)
+    return prefix if is_it_zero?(remaining_number)
+    get_two_digits_numeral(remaining_number, prefix: prefix)
+  end
+
+  #========================= End: Low level Flow methods =====================================================
+
+  #========================= Begin: Utility methods ==========================================================
 
   def prefix_for_three_digits_number(number_for_prefix)
     "#{NUMERALS[number_for_prefix][:singular]} hundred" unless is_it_zero?(number_for_prefix)
   end
 
-  def prefixed_three_digit_number(prefix, remaining_number)
-    return prefix if is_it_zero?(remaining_number)
-    get_two_digits_numeral(number: remaining_number, prefix: prefix)
+  def number_with_separator(number, prefix: _prefix, separator: _separator)
+    "#{prefix}#{separator}#{number}"
   end
 
-  def define_two_digit_number(number)
-    root = NUMERALS[number[0].to_i][:root]
-    return root if is_it_zero?(number[1])
-    parse_two_digits_with_root(root, number)
+  def parse_two_digits_with_root(number, root: _root)
+    specific_tens_number = get_one_digit_numeral(number[1])
+    return number_with_separator(specific_tens_number, prefix: root, separator: "-") if root
+    specific_tens_number
   end
 
- #This, in order to check if the value it's 00, meaning, there it's not any special case
-  def verify_special_cases(number)
-    plural_number = TWO_DIGITS_NUMERALS[number.to_i]
-    return plural_number if plural_number
-    define_two_digit_number(number)
+  def number_delimiter_for(delimiter_level)
+    #This it's because the hundre delimiter it's already added at the three digits parsed flow
+    if !is_it_zero?(delimiter_level)
+      DELIMITERS[delimiter_level - 1]
+    end
+  end
+
+  def was_a_negative_number?(array)
+    #Replace 'not' for '!' because higher precedence
+    if !is_it_zero?(self) && self.to_s.include?("-")
+      last_position = array[-1]
+      array[-1] = "negative #{last_position}"
+    end
   end
 
   def is_it_zero?(number)
@@ -100,25 +122,11 @@ module Arabic2English
     is_it_zero?(number) && get_numbers_collection.count > 1
   end
 
-  def parse_two_digits_with_root(root, number)
-    if root
-      "#{root}-#{get_one_digit_numeral(number: number[1].to_i)}"
-    else
-      get_one_digit_numeral(number: number[1].to_i)
-    end
+  def get_numbers_collection
+    self.to_s.reverse.each_char.each_slice(3).map {|sliced_chars| sliced_chars.reverse.join }
   end
 
-  def number_delimiter_for(delimiter_level)
-    DELIMITERS[delimiter_level]
-  end
-
-  def was_a_negative_number?(array)
-    if (not is_it_zero?(self)) && self.to_s.include?("-")
-      last_position = array[-1]
-      array[-1] = "negative #{last_position}"
-    end
-  end
-
+  #========================= End: Utility methods ==========================================================
 end
 
 class Integer
